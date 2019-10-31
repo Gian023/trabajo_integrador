d0290490388840c99a1c27724136c4e8a07629ea
Gian <gpezzi@udesa.edu.ar>
2019-10-31 00:27:42 -0300

    Hice el Api (todavia no funciona bien)

--- js/java.js
+++ js/java.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -16,4 +17,43 @@ window.onclick = function(event) {
       }
     }
   }
+}
+// Api's
+fetch("https://api.themoviedb.org/3/discover/tv?api_key=c0e01d0df95b98b689dcb3af16007742&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false")
+  .then(function(respuesta){
+    return respuesta.json();
+  })
+  .then(function(datos){
+    console.log(datos);
+     var ul = document.querySelector("#pelis");
+     var pel = datos.results;
+
+     for (var i=0; i<pel.length; i++){
+       ul.innerHTML += '<li>' + '<img src="' + datos.results[i].backdrop_path + '">' + '</li>'
+     }
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
