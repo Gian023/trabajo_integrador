e409e4582b6b773442e2a073e1c135772bbf24fb
Gian <gpezzi@udesa.edu.ar>
2019-11-08 15:50:34 -0300

    hice el título

--- js/detalle_pelis.js
+++ js/detalle_pelis.js
@@ -17,11 +17,12 @@ fetch("https://api.themoviedb.org/3/tv/" + id + "?api_key=c0e01d0df95b98b689dcb3
 })
 .then(function(datos){
  console.log(datos);
-  // var div = document.querySelector(".gnre");
+  var info_conteiner = document.querySelector(".info_conteiner");
   // var gnre = datos.generes;
-
+  info_conteiner.innerHTML += '<h1>'+  datos.name + '</h1>'
+  // info_conteiner.innerHTML += '<li><a href="#">'+  datos.generes[i].name + '</a>+ </li>'
   // for (var i=1; i<16; i++){
-  //   div.innerHTML += '<li><a href="#">'+  datos.generes[i].name + '</a>+ </li>'
+  //
   // }
 })
 
