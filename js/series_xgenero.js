a3a13a8d439c7eaf7693b16975e6bbc4564bc311
Francisco <ffranciscocasas@gmail.com>
2019-11-15 18:14:35 -0300

    ARREGLE Y METI SCROLL INFINITOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- js/series_xgenero.js
+++ js/series_xgenero.js
@@ -4,10 +4,13 @@ window.onload = function(){
 var querystring = location.search;
 var query2 = new URLSearchParams(querystring)
 var id = query2.get("id")
-
+var page = 1;
+vermas()
+function vermas() {
 // GENEROSSSSSSS
+var url = "https://api.themoviedb.org/3/discover/tv?api_key=c0e01d0df95b98b689dcb3af16007742&language=en-US&sort_by=popularity.desc&page=2" + page + "&timezone=America%2FNew_York&include_null_first_air_dates=false&with_genres=" + id;
 console.log("hola mundo!");
-fetch("https://api.themoviedb.org/3/discover/tv?api_key=c0e01d0df95b98b689dcb3af16007742&language=en-US&sort_by=popularity.desc&page=2&timezone=America%2FNew_York&include_null_first_air_dates=false&with_genres=" + id )
+fetch(url)
 .then(function(respuesta){
  return respuesta.json();
 })
@@ -31,8 +34,29 @@ console.log(series.length);
     titulo.style.textAlign = "center"
     titulo.style.color = "Red"
   }
+  for(var i=0; i<series.length; i++){
+    div.innerHTML+= '<li><a href="info_serie.html?id='+ datos.results[i].id+'"> '+'<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
+  }
+  if (datos.total_pages == page) {
+    console.log('cortamo');
+    window.removeEventListener('scroll', scrolled)
+    return
+    // alert("No se encuentran resultados")
+  }
+  })
 
-})
+  }
+  window.addEventListener('scroll', scrolled)
+  function scrolled(e) {
+  var myDiv = document.querySelector('body')
+  if (myDiv.offsetHeight + myDiv.scrollTop >= myDiv.scrollHeight) {
+  // scrolledToBottom(e);
+  page++
+  vermas()
+  }
+
+  }
+
 
 
 
