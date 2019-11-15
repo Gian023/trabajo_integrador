0eb42cf6affc0557bd6647449f45b19234e1ce32
Francisco <ffranciscocasas@gmail.com>
2019-11-15 16:38:20 -0300

    Haciendo formulario

--- js/avanzado.js
+++ js/avanzado.js
@@ -1,27 +1,18 @@
-window.addEventListener("load",function(){
-var queryString = location.search; //Capturamos la query string del navegador
+window.onload = function (){
+  console.log("Hola puto");
+  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=c0e01d0df95b98b689dcb3af16007742&language=en-US")
+  .then(function(respuesta){
+   return respuesta.json();
+  })
+  .then(function(datos){
+   console.log(datos);
+    var buscador = document.querySelector(".avana");
+    var form = document.createElement("select");
+    buscador.append(form);
+    var gnre = datos.genres;
 
-var searchParams = new URLSearchParams(queryString); //Obtenemos las posiciones y los datos de la queryString
-
-var busqueda = searchParams.get("busqueda"); //con el método get obtenenemos el valor del término a buscar. En este obtenenemos lo que escribió el usuario en el campo de busqueda cuyo "name" es "search" (name="search").
-  var url = "https://api.themoviedb.org/3/search/tv?api_key=7246c48f98d8db92d443b21af0633a14&language=en-US&query=" + busqueda + '&page=1'
-
-    fetch(url)
-      .then(function(respuesta){
-        return respuesta.json();
-      })
-      .then(function(datos){
-      var destino = document.querySelector(".resultados");
-      var datosFinales = datos.results;
-      var titulo = document.querySelector(".primero");
-
-      titulo.innerText = busqueda;
-
-      console.log(datos);
-
-      for(var i=0; i<datosFinales.length; i++){
-        destino.innerHTML+= '<li><a href="info_serie.html?id='+ datos.results[i].id+'"> '+'<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
-      }
-    })
-
-});
+    for (var i=0; i<gnre.length; i++){
+      form.innerHTML += '<option><a href="series_xgenero.html?id='+ datos.genres[i].id +'&name='+ datos.genres[i].name +'">'+  datos.genres[i].name + '</a></option>'
+    }
+  })
+}
