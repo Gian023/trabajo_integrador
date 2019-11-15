6bcd576c17100d126c0f1ac64003b86bfadd8955
Francisco <ffranciscocasas@gmail.com>
2019-11-15 15:46:42 -0300

    Intento scroll infinito

--- js/avanzado.js
+++ js/avanzado.js
@@ -4,8 +4,7 @@ var queryString = location.search; //Capturamos la query string del navegador
 var searchParams = new URLSearchParams(queryString); //Obtenemos las posiciones y los datos de la queryString
 
 var busqueda = searchParams.get("busqueda"); //con el método get obtenenemos el valor del término a buscar. En este obtenenemos lo que escribió el usuario en el campo de busqueda cuyo "name" es "search" (name="search").
-var page = 1;
-  var url = "https://api.themoviedb.org/3/search/tv?api_key=7246c48f98d8db92d443b21af0633a14&language=en-US&query=" + busqueda + '&page='+ page
+  var url = "https://api.themoviedb.org/3/search/tv?api_key=7246c48f98d8db92d443b21af0633a14&language=en-US&query=" + busqueda + '&page=1'
 
     fetch(url)
       .then(function(respuesta){
@@ -24,13 +23,5 @@ var page = 1;
         destino.innerHTML+= '<li><a href="info_serie.html?id='+ datos.results[i].id+'"> '+'<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
       }
     })
-
-// var btn = document.querySelector("#page");
-// btn.onclick =  function boton () {
-//   page += 1;
-// }
-
-
-
 
 });
