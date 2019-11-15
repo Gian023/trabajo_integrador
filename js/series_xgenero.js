4b2b8237c66787bdc82deae49573625d909c1179
Gian <gpezzi@udesa.edu.ar>
2019-11-15 15:31:51 -0300

    puch

--- js/series_xgenero.js
+++ js/series_xgenero.js
@@ -15,31 +15,30 @@ fetch("https://api.themoviedb.org/3/discover/tv?api_key=c0e01d0df95b98b689dcb3af
  console.log(datos);
   var div = document.querySelector(".ul_generos");
   var series = datos.results;
+console.log(series.length);
 
   for (var i=0; i<series.length; i++){
     div.innerHTML += '<li><a id="click_pelis" href="info_serie.html?id='+ datos.results[i].id +'"> ' + '<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
   }
+
+  if (datos.results.length=0) {
+    alert("Hola")
+    UIkit.notification({
+    message: 'No hay resultados',
+    status: 'primary',
+    pos: 'top-center',
+    timeout: 3000
+    });
+    console.log("HOLA");
+
+  }
+
 })
 
-
+//titulo
 var titulo = document.querySelector("#titulos");
 var name = query2.get("name")
-console.log(name);
-titulo.innerHTML = name
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+titulo.innerHTML = name;
 
 
 
