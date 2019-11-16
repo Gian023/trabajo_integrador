336a6cbb8d9ba0ae711597142be33e638ac296c3
Gian <gpezzi@udesa.edu.ar>
2019-11-16 14:33:58 -0300

    ul generos

--- js/generos.js
+++ js/generos.js
@@ -9,7 +9,7 @@ fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=c0e01d0df95b98b689dcb3
 })
 .then(function(datos){
  console.log(datos);
-  var div = document.querySelector(".generos_container");
+  var div = document.querySelector(".generos_container ul");
   var gnre = datos.genres;
 
   for (var i=0; i<gnre.length; i++){
