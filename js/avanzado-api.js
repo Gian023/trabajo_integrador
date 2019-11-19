efe4b23f5d2d614a6264ff217464c00d45606276
Gian <gpezzi@udesa.edu.ar>
2019-11-19 17:14:21 -0300

    logre la validacion!!!!!

--- js/avanzado-api.js
+++ js/avanzado-api.js
@@ -1,5 +1,8 @@
-window.addEventListener("load", function() {
-  console.log("Hola puto");
+// window.addEventListener("load", function() {
+  window.onload = function(){
+
+
+  console.log("Hola ");
   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=c0e01d0df95b98b689dcb3af16007742&language=en-US")
     .then(function(respuesta) {
       return respuesta.json();
@@ -9,8 +12,9 @@ window.addEventListener("load", function() {
       var buscador = document.querySelector(".avana");
       var label1 = document.querySelector(".incluir");
       var label2 = document.querySelector(".excluir");
-      // buscador.append(form);
+
       var gnre = datos.genres;
+
 
       for (var i = 0; i < gnre.length; i++) {
         label1.innerHTML += '<option value="' + datos.genres[i].id + '">' + datos.genres[i].name + '</option>'
@@ -18,7 +22,57 @@ window.addEventListener("load", function() {
       }
     })
 
+//validacion
+var form = document.querySelector(".avana");
+var select1 = document.querySelector(".incluir");
+var select2 = document.querySelector(".excluir");
+var orden = document.querySelector(".orden");
+var input = document.querySelector("input[name=año]");
+console.log("------------");
+
+var option1Selected = select1.selectedIndex;
+var option2Selected = select2.selectedIndex;
+console.log(select1.options);
+console.log(option1Selected);
+
+// select1.addEventListener("change", function(){
+//   console.log(this.options[this.selectedIndex].value);
+//   if (this.options[this.selectedIndex].value == 0) {
+//     alert("hola")
+//   }
+// });
+
+form.onsubmit = function(event){
+  if (select1.options[select1.selectedIndex].value == 0 && select2.options[select2.selectedIndex].value == 0 && input.value == "") {
+    event.preventDefault();
+    UIkit.notification({
+      message: 'Complete algun campo',
+      status: 'primary',
+      pos: 'top-center',
+      timeout: 3000
+    });
+  }
+  else if (select1.options[select1.selectedIndex].value == select2.options[select2.selectedIndex].value) {
+    event.preventDefault();
+    UIkit.notification({
+      message: 'Los generos no pueden ser iguales',
+      status: 'primary',
+      pos: 'top-center',
+      timeout: 3000
+    });
+  }
+
+  // select1.addEventListener("change", function(){
+  //   console.log(this.options[this.selectedIndex].value);
+  //   if (this.options[this.selectedIndex].value == 0) {
+  //     alert("hola")
+  //     event.preventDefault();
+  //   }
+  // });
+}
 
 
 
-});
+
+
+};
