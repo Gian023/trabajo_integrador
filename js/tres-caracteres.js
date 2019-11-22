121144a3bf6c4c08329fce70a87233c1049b004a
Gian <gpezzi@udesa.edu.ar>
2019-11-22 12:32:15 -0300

    funcionó

--- js/tres-caracteres.js
+++ js/tres-caracteres.js
@@ -27,6 +27,8 @@ var logForm = document.querySelector(".LogIn")
 var nombre = document.querySelector("[name=nombre]")
 var email = document.querySelector("[name=email]")
 
+var nombreUsuario = "";
+
 // validacion email
 logForm.onsubmit = function(event){
 var regaxEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -58,21 +60,36 @@ else if (regaxEmail.test(email.value) == false) {
    });
 }
   guardarUsuarios()
+
+}
   function guardarUsuarios(){
     localStorage.setItem('nombre', nombre.value)
     localStorage.setItem('email', email.value)
-
   }
   obtenerUsuarios()
   function obtenerUsuarios(){
-    var nombreStorage = localStorage.getItem("nombre")
+  nombreUsuario = localStorage.getItem("nombre")
   }
-  alert("hola " + nombreStorage)
-}
-if (true) {
 
-}
+ var logOUTButton = document.querySelector("#logOut")
 
+  if (nombreUsuario != "") {
+    // UIkit.notification({
+    //   message: 'Bienvenido ' + nombreUsuario,
+    //   status: 'primary',
+    //   pos: 'top-center',
+    //   timeout: 5000
+    //  });
+    document.querySelector("#a_usuario").style.display = "none"
+    logOUTButton.style.display = "block"
+  }
+
+  logOUTButton.onclick = function(){
+    if (logOUTButton.style.display == "block" ) {
+      document.querySelector("#a_usuario").style.display = "block"
+      document.querySelector("#logOut").style.display = "none"
+    }
+  }
 
 
 
